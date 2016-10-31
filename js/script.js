e9dbc835c0bbcf4bef6e33f06abf5f6bfd1502d5
juandi14 <juandi310@gmail.com>
2016-10-30 23:22:22 -0400

    first revision

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-	
-}
+
+    $(".reveal").click(function() {
+    	event.preventDefault();
+    	$(".resume-text").slideToggle();
+    	})
+
+     // $(".mobile-menu").click(function() {
+   //         $("#dropdown").slideToggle();
+   //    }
+   //);
+
+}); 
