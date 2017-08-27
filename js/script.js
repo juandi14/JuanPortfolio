ae7877c90334c7ef56c7dac6a889e6d02c544fcb
Juan Henao <juandi310@gmail.com>
2017-08-27 17:27:18 -0400

    fixed bugs, updated resume

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
-	
-}
+
+    $(".reveal").click(function() {
+    	event.preventDefault();
+    	$(".resume-text").fadeToggle("slow");
+    	})
+
+    $(".hide").click(function() {
+        event.preventDefault();
+        $(".resume-text").slideToggle("slow");
+    })
+
+    $(".hamburger").click(function() {
+        $("#dropdown").slideToggle("slow");
+   	   }
+   );
+
+    $(".scroll-up").click(function() {
+    	$("html, body").animate({scrollTop: 0}, "slow");
+    })
+
+	$(document).scroll(function() {
+	  var y = $(this).scrollTop();
+	  if (y >280) {
+	    $('.scroll-up').fadeIn();
+	  } else {
+	    $('.scroll-up').fadeOut();
+	  }
+
+	});
+})
+
